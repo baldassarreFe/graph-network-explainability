79765774e5bf2c0fe3523709f38d49952c34eacb
Federico Baldassarre <baldassarre.fe@gmail.com>
2019-03-13 15:02:18 +0100

    Small improvements for saver and utils

--- src/saver.py
+++ src/saver.py
@@ -6,9 +6,9 @@ from torch import nn, optim
 
 
 class Saver(object):
-    def __init__(self, folder: Path):
-        self.folder = folder
-        self.folder.mkdir(parents=True)
+    def __init__(self, folder: Union[str, Path]):
+        self.folder = Path(folder)
+        self.folder.mkdir(parents=True, exist_ok=True)
 
     def save(self, *, id_: str = '', model: Optional[nn.Module] = None, optimizer: Optional[optim.Optimizer] = None,
              **rest):
@@ -32,24 +32,3 @@ class Saver(object):
             model.load_state_dict(rest['optimizer'])
             del rest['optimizer']
         return rest
-
-    '''
-    def save_model(self, model: nn.model, id=''):
-        if isinstance(model, nn.DataParallel):
-            model = model.module
-        torch.save(model.state_dict(), self.folder / f'model_{id}.pt')
-
-    def load_model(self, model: nn.model, id='', device=None):
-        state_dict = torch.load(self.folder / f'model_{id}.pt', map_location=device)
-        model.load_state_dict(state_dict)
-        return model.to(device)
-
-    def save_training(self, optimizer: optim.Optimizer, **kwargs):
-        torch.save({'_optimizer': optimizer.state_dict(), **kwargs}, self.folder / f'training_{id}.pt')
-
-    def load_training(self, optimizer: optim.Optimizer):
-        loaded = torch.load(self.folder / f'training_{id}.pt')
-        optimizer.load_state_dict(loaded['_optimizer'])
-        del loaded['_optimizer']
-        return optimizer, loaded
-    '''
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,14 @@
-import importlib
-
-
 def load_class(fullname):
+    import importlib
     package, name = fullname.rsplit('.', maxsplit=1)
     package = importlib.import_module(package)
     return getattr(package, name)
+
+
+def set_seeds(seed):
+    import random
+    import torch
+    import numpy as np
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.random.manual_seed(seed)
